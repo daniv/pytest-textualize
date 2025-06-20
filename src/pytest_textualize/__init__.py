6aade8d03c34f666e7c13cdb19454e51be6941fe
Dani Vainstein <solmarkn@gmail.com>
2025-06-20 04:13:03 -0500

    added pytest reporting hooks implementation

--- src/pytest_textualize/__init__.py
+++ src/pytest_textualize/__init__.py
@@ -1,22 +1,71 @@
 from __future__ import annotations
 
 from importlib import metadata
+import logging
+from typing import TYPE_CHECKING
+
+from rich.console import Console
+from rich.logging import RichHandler
 
 __version__ = metadata.version("pytest_textualize")
 
 from pytest_textualize.console_factory import ConsoleFactory
 from pytest_textualize.helpers import get_bool_opt
-from pytest_textualize.settings import ConsoleSettings
+from pytest_textualize.settings import ConsolePyProjectSettings
 from pytest_textualize.settings import TextualizeSettings
-from pytest_textualize.settings import TracebacksSettingsModel
-from pytest_textualize.settings import LoggingSettingsModel
+from pytest_textualize.settings import TracebacksPyProjectSettings
+from pytest_textualize.settings import LoggingPyProjectSettings
 
 __all__ = [
     "TextualizeSettings",
     "ConsoleFactory",
-    "ConsoleSettings",
-    "TracebacksSettingsModel",
-    "LoggingSettingsModel",
+    "ConsolePyProjectSettings",
+    "TracebacksPyProjectSettings",
+    "LoggingPyProjectSettings",
     "get_bool_opt",
+    "reset_logging",
+    "configure_logging",
     "__version__",
 ]
+
+if TYPE_CHECKING:
+    from pytest import Config
+
+
+def reset_logging() -> None:
+    # -- Remove all handlers from the root logger
+    root = logging.getLogger()
+    for h in root.handlers[:]:
+        root.removeHandler(h)
+    # -- Reset logger hierarchy, this clears the internal dict of loggers
+    logging.Logger.manager.loggerDict.clear()
+
+
+def configure_logging(console: Console, txt_settings: TextualizeSettings) -> None:
+    log_file_handler: logging.FileHandler | None = None
+
+    log_level = txt_settings.logging_settings.level
+    if isinstance(log_level, str):
+        log_level = logging.getLevelName(log_level)
+    try:
+        from rich.logging import RichHandler
+        from .textualize.logging import TextualizeConsoleLogRender, TextualizeConsoleHandler
+        logging_options = txt_settings.logging_settings.model_dump(exclude_unset=True, exclude={"level"})
+        rich_handler = TextualizeConsoleHandler(log_level, console, **logging_options)
+    except ValueError as e:
+        if str(e).startswith("Unknown level"):
+            from pytest import UsageError
+            raise UsageError(
+                f"'{log_level}' is not recognized as a logging level name for "
+                f"'log_level'. Please consider passing the logging level num instead."
+            ) from e
+        raise e from e
+
+    reset_logging()
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format=txt_settings.log_format,
+        datefmt=txt_settings.logging_settings.log_time_format,
+        handlers=[rich_handler],
+    )
+    logging.captureWarnings(True)
