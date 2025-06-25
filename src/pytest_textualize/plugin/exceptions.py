105ac3072a5230c3f57d7adb0b6fc675c827b224
Dani Vainstein <solmarkn@gmail.com>
2025-06-25 14:04:50 -0500

    before moving to base model

--- src/pytest_textualize/plugin/exceptions.py
+++ src/pytest_textualize/plugin/exceptions.py
@@ -4,27 +4,23 @@
 from __future__ import annotations
 
 import shlex
-import sys
+from contextlib import suppress
 from dataclasses import InitVar
 from dataclasses import dataclass
 from dataclasses import field
 from subprocess import CalledProcessError
 from typing import Self
 from typing import TYPE_CHECKING
-from contextlib import suppress
 
-import pytest
-from _pytest.config import ExitCode
+from pydantic import BaseModel
+from rich.panel import Panel
 from rich.text import Text
 
 from pytest_textualize import Verbosity
-from pytest_textualize.helpers import Comparable
 from pytest_textualize.textualize.console import is_markup
 
 if TYPE_CHECKING:
     from rich.console import Console
-    from rich.text import TextType
-
 
 
 def decode(string: bytes | str, encodings: list[str] | None = None) -> str:
@@ -44,8 +40,8 @@ class TextualizeError(Exception):
     pass
 
 
-@dataclass
-class ConsoleMessage:
+# @dataclass
+class ConsoleMessage(BaseModel):
     """
     Representation of a console message, providing utilities for formatting text
     with tags, indentation, and sections.
@@ -58,6 +54,10 @@ class ConsoleMessage:
     text: str
     debug: bool = False
 
+    def escape(self) -> Self:
+        self.text = self.text.replace("[", "\\[")
+        return self
+
     @property
     def stripped(self) -> str:
         if is_markup(self.text):
@@ -65,9 +65,10 @@ class ConsoleMessage:
             return txt.plain
         return self.text
 
-    def style(self, tag: str) -> Self:
+    def style(self, name: str) -> Self:
         if self.text:
-            self.text = Text(self.text, style=tag).markup
+            self.text = f"[{name}]{self.text}[/{name}]"
+            pass
         return self
 
     def indent(self, indent: str) -> ConsoleMessage:
@@ -105,11 +106,11 @@ class PrettyCalledProcessError:
         output: A section formatted representation of the exception stdout.
         errors: A section formatted representation of the exception stderr.
         command_message: Formatted message including a hint on retrying the original command.
-        command: A `shelex` quoted string representation of the original command.
+        command: A `shlex` quoted string representation of the original command.
         exception: The original `CalledProcessError` instance.
         indent: Indent prefix to use for inner content per section.
     """
-
+    doc: ConsoleMessage = field(init=False)
     message: ConsoleMessage = field(init=False)
     output: ConsoleMessage = field(init=False)
     errors: ConsoleMessage = field(init=False)
@@ -119,6 +120,7 @@ class PrettyCalledProcessError:
     indent: InitVar[str] = field(default="")
 
     def __post_init__(self, exception: CalledProcessError, indent: str) -> None:
+        self.doc = ConsoleMessage(exception.__doc__, debug=True).make_section("Exception doc", indent)
         self.message = ConsoleMessage(str(exception).strip(), debug=True).make_section("Exception", indent)
         self.output = ConsoleMessage(decode(exception.stdout), debug=True).make_section("Output", indent)
         self.errors = ConsoleMessage(decode(exception.stderr), debug=True).make_section("Errors", indent)
@@ -127,26 +129,20 @@ class PrettyCalledProcessError:
             if isinstance(exception.cmd, list)
             else exception.cmd
         )
-        self.command_message = ConsoleMessage(
-            f"You can test the failed command by executing:\n\n    <c1>{self.command}</c1>",
-            debug=False,
-        )
-        
+
+
 class TextualizeRuntimeError(TextualizeError):
-    """
-    Represents a runtime error in the pytest-textualize console application.
-    """
-    """
-    Represents a runtime error in the Poetry console application.
-    """
+    """Represents a runtime error in the pytest-textualize console application."""
 
     def __init__(
         self,
         reason: str,
+        exc_typename: str,
         messages: list[ConsoleMessage] | None = None,
         exit_code: int = 1,
     ) -> None:
         super().__init__(reason)
+        self.exc_typename = exc_typename
         self.exit_code = exit_code
         self._messages = messages or []
         self._messages.insert(0, ConsoleMessage(reason))
@@ -157,7 +153,9 @@ class TextualizeRuntimeError(TextualizeError):
         """
         debug = verbose > Verbosity.NORMAL
         if text := self.get_text(debug=debug, strip=False):
-            error_console.print(text)
+            p = Panel(Text.from_markup(text), expand=False, title=f"error type: {self.exc_typename}", padding=1)
+            error_console.print(p, style="bright_red")
+            # error_console.print(Text.from_markup(text), style="bright_red")
 
     def get_text(
         self, debug: bool = False, indent: str = "", strip: bool = False
@@ -181,6 +179,7 @@ class TextualizeRuntimeError(TextualizeError):
             if indent:
                 message_text = f"\n{indent}".join(message_text.splitlines())
 
+            # text += f"{indent}{message_text}\n{indent}\n"
             text += f"{indent}{message_text}\n{indent}\n"
 
         if has_skipped_debug:
@@ -188,6 +187,7 @@ class TextualizeRuntimeError(TextualizeError):
                 f"{indent}You can also run your [#9EC6F3]pytest[/] command with [#9EC6F3]-v|-vv|-vvv[/] to see more information.\n{indent}\n"
             )
             text += message.stripped if strip else message.text
+            pass
 
         return text.rstrip(f"{indent}\n")
 
@@ -201,15 +201,12 @@ class TextualizeRuntimeError(TextualizeError):
         exception: CalledProcessError | Exception | None = None,
         info: list[str] | str | None = None,
     ) -> Self:
-        """
-        Create an instance of this class using the provided reason. If
-        an exception is provided, this is also injected as a debug
-        `ConsoleMessage`.
+        """Create an instance of this class using the provided reason. If
+        an exception is provided, this is also injected as a debug `ConsoleMessage`.
 
         There is specific handling for known exception types. For example,
         if exception is of type `subprocess.CalledProcessError`, the following
-        sections are additionally added when available - stdout, stderr and
-        command for testing.
+        sections are additionally added when available - stdout, stderr and command for testing.
         """
         if isinstance(info, str):
             info = [info]
@@ -221,11 +218,12 @@ class TextualizeRuntimeError(TextualizeError):
         if isinstance(exception, CalledProcessError):
             error = PrettyCalledProcessError(exception, indent="    | ")
             messages = [
-                error.message.style("logging.level.warning"),
-                error.output.style("logging.level.warning"),
-                error.errors.style("logging.level.warning"),
+                error.doc.style("white"),
+                error.message,
+                error.output.style("#A6AEBF"),
+                error.errors,
                 *messages,
-                error.command_message,
+                # error.command_message,
             ]
         elif exception is not None and isinstance(exception, Exception):
 
@@ -235,8 +233,7 @@ class TextualizeRuntimeError(TextualizeError):
                     f"[scope.key]description: [/][#c6c688]{exception.__doc__}[/]\n[scope.key]message: [/]{str(exception)}", debug=True
                 ).make_section(f"[#8888C6]{type(exception).__name__}[/]", indent="    | "),
             )
-
-        return cls(reason, messages)
+        return cls(reason, repr(type(exception)), messages)
 
     def append(self, message: str | ConsoleMessage) -> Self:
         if isinstance(message, str):
